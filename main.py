56a48954a75fd4bcc63b21dcef73d204529f5ea4
Olívia <olivialauarg@gmail.com>
2024-06-27 20:45:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,35 +3,35 @@ import seaborn as sns
 
 class Gym:
     def __init__(self):
-        self.dumbbels = [i for i in range(10, 38) if i % 2 == 0]
-        self.dumbbels_holder = {}
+        self.dumbbells = [i for i in range(10, 38) if i % 2 == 0]
+        self.dumbbells_holder = {}
         self.restart_the_day()
 
     def restart_the_day(self):
-        self.dumbbels_holder = {i: i for i in self.dumbbels}
+        self.dumbbells_holder = {i: i for i in self.dumbbels}
     
-    def lists_dumbbels(self):
-        return [i for i in self.dumbbels_holder.values() if i != 0]
+    def lists_dumbbells(self):
+        return [i for i in self.dumbbells_holder.values() if i != 0]
     
     def lists_spots(self):
-        return [i for i, j in self.dumbbels_holder.items() if j == 0]
+        return [i for i, j in self.dumbbells_holder.items() if j == 0]
     
-    def take_dumbbels(self, weight):
-        if weight in self.dumbbels_holder.values():
-            dumbbels_position = list(self.dumbbels_holder.values()).index(weight)
-            key_to_dumbbels = list(self.dumbbels_holder.keys())[dumbbels_position]
-            self.dumbbels_holder[key_to_dumbbels] = 0
+    def take_dumbbells(self, weight):
+        if weight in self.dumbbells_holder.values():
+            dumbbells_position = list(self.dumbbells_holder.values()).index(weight)
+            key_to_dumbbells = list(self.dumbbells_holder.keys())[dumbbells_position]
+            self.dumbbells_holder[key_to_dumbbells] = 0
             return weight
         else:
             return None
     
-    def return_dumbbels(self, position, weight):
-        if position in self.dumbbels_holder:
-            self.dumbbels_holder[position] = weight
+    def return_dumbbells(self, position, weight):
+        if position in self.dumbbells_holder:
+            self.dumbbells_holder[position] = weight
 
     def how_messy(self):
-        messy_number = [i for i, j in self.dumbbels_holder.items() if i != j and j != 0]
-        return len(messy_number) / len(self.dumbbels_holder)
+        messy_number = [i for i, j in self.dumbbells_holder.items() if i != j and j != 0]
+        return len(messy_number) / len(self.dumbbells_holder)
 
 
 class User:
@@ -41,24 +41,24 @@ class User:
         self.weight = 0
 
     def start_training(self):
-        weights_list = self.gym.lists_dumbbels()
+        weights_list = self.gym.lists_dumbbells()
         if weights_list:
             self.weight = random.choice(weights_list)
-            self.gym.take_dumbbels(self.weight)
+            self.gym.take_dumbbells(self.weight)
 
     def end_training(self): 
         spots = self.gym.lists_spots()
 
         if self.type == 1:
             if self.weight in spots:
-                self.gym.return_dumbbels(self.weight, self.weight)
+                self.gym.return_dumbbells(self.weight, self.weight)
             else:
                 position = random.choice(spots)
-                self.gym.return_dumbbels(position, self.weight)
+                self.gym.return_dumbbells(position, self.weight)
 
         if self.type == 2:
             position = random.choice(spots)
-            self.gym.return_dumbbels(position, self.weight)
+            self.gym.return_dumbbells(position, self.weight)
         self.weight = 0
 
 gym = Gym()
@@ -78,7 +78,7 @@ for k in range(50):
             user.end_training()
     messy_list.append(gym.how_messy())
 
-print(gym.dumbbels_holder)
+print(gym.dumbbells_holder)
 print(gym.how_messy())
 
 sns.displot(messy_list)
